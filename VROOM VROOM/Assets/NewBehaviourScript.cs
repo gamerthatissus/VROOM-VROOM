5fd8fa3261b2e2174193b951979b11aa772752cd
gamerthatissus <123601491+gamerthatissus@users.noreply.github.com>
2025-06-05 21:16:42 -0700

    e
    e

--- VROOM VROOM/Assets/NewBehaviourScript.cs
+++ VROOM VROOM/Assets/NewBehaviourScript.cs
@@ -2,8 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class NewBehaviourScript : MonoBehaviour
 {
+    public Rigidbody rb;
+    public 
     // Start is called before the first frame update
     void Start()
     {
